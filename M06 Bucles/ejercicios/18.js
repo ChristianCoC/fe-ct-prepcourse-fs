15952dce4315cf430dba5432cae29812cf43c1b4
ChristianCoC <christianelmillo81@gmail.com>
2023-12-27 14:39:01 -0300

    Modulo 06 completado, hay algunos errores en el test

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -2,6 +2,9 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  for (let i = a; i <= b; i++) {
+    console.log(i);
+  }
 }
 
 module.exports = productoEntreNúmeros;
